6837e4fd1635d52b9e5dd8c2bb2c7353081702ad
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-05-27 14:08:54 -0700

    Feature: Search w/ URL query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,11 @@ export default async function Home({
 	searchParams: Promise<{ query?: string }>;
 }) {
 	const query = (await searchParams).query;
-	const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+	const params = { search: query || null };
+	const { data: posts } = await sanityFetch({
+		query: STARTUPS_QUERY,
+		params,
+	});
 
 	return (
 		<>
@@ -41,7 +45,7 @@ export default async function Home({
 					)}
 				</ul>
 			</section>
-			{console.log(posts)}
+
 			<SanityLive />
 		</>
 	);
